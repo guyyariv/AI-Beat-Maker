32f811d6f78239d79e3d1029451231dd28d69973
Guyar <guyyariv111@gmail.com>
2021-10-08 17:46:34 +0300

    Update

--- main.py
+++ main.py
@@ -5,29 +5,20 @@ from matplotlib import pyplot as plt
 import numpy as np
 import IPython.display as ipd
 import librosa
-import librosa.display
 import soundfile as sf
+from scipy.ndimage import filters
+import libfmp.b
+import libfmp.c2
+import libfmp.c6
+import utils
+import os, sys
+from numba import jit
+from beat_tracking.onset_detection import OnsetDetection
 
-
-def plotting_spectogram(filename):
-    sample_rate, samples = wav.read(filename)
-    f, t, Zxx = signal.stft(samples, fs=sample_rate)
-    plt.pcolormesh(t, f, np.abs(Zxx))
-    plt.specgram(samples, Fs=sample_rate)
-    plt.show()
-
-
-def beat_tracking(filename):
-    x, sr = librosa.load(filename)
-    ipd.Audio(x, rate=sr)
-    tempo, beat_times = librosa.beat.beat_track(x, sr=sr, start_bpm=60,
-                                                units='samples')
-    plt.figure(figsize=(14, 5))
-    x_ = x[beat_times[0]:beat_times[2]]
-    librosa.display.waveplot(x_, alpha=0.6)
-    plt.show()
-    sf.write('reports/new.wav', x_, sr)
+sys.path.append('..')
 
 
 if __name__ == '__main__':
-    beat_tracking('resources/file_example_WAV_10MG.wav')
+    x, sr = librosa.load('resources/test.wav')
+    onset_detection = OnsetDetection()
+    pass
