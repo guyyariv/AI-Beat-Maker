bfc3eac69adfe0ab2aeb1a74eac11a45b6c12917
Guyar <guyyariv111@gmail.com>
2021-10-24 16:52:13 +0300

    adding finally beat tracking algorithm

--- main.py
+++ main.py
@@ -6,44 +6,98 @@ import numpy as np
 import IPython.display as ipd
 import librosa
 import soundfile as sf
-from scipy.ndimage import filters
-import libfmp.b
-import libfmp.c2
-import libfmp.c6
+import random
 import utils
 import os, sys
-from numba import jit
 from beat_tracking.onset_detection import OnsetDetection
 
 sys.path.append('..')
 
 
-def beat_tracking(sec_duration, onset_envelope, tempo_estimation=None):
-    if tempo_estimation == None:
-        tempo_estimation = librosa.beat.tempo(onset_envelope=onset_envelope,
-                                              aggregate=None)
-    if tempo_estimation[np.insert(np.diff(tempo_estimation).astype(
-            np.bool), 0, True)].size < sec_duration / 4:
-        (_, idx, counts) = np.unique(tempo_estimation, return_index=True, return_counts=True)
-        index = idx[np.argmax(counts)]
-        mode = tempo_estimation[index]
-        return librosa.beat.beat_track(onset_envelope=onset_envelope, bpm=mode,
-                                       units='time')
-    else:
-        pulse = librosa.beat.plp(onset_envelope=onset_envelope)
-        beats_plp = np.flatnonzero(librosa.util.localmax(pulse))
-        beats = librosa.times_like(pulse)
-        return tempo_estimation, beats[beats_plp]
+def beat_tracking_and_slice(audio_data, sample_rate):
+    # spectral based onset envelop
+    onset_env = librosa.onset.onset_strength(y=audio_data, sr=sample_rate)
+    #
+    times = librosa.times_like(onset_env, sr=sample_rate)
+    # onset_frames = librosa.onset.onset_detect(onset_envelope=onset_envelope, sr=sample_rate)
+    # X_stft = librosa.stft(audio_data)
 
+    # estimate tempo from onset correlation
+    # pick peaks in onset strength approximately consistent with estimated
+    # tempo
+    tempo, beats = librosa.beat.beat_track(onset_envelope=onset_env)
+    # find a locally stable tempo for each frame
+    pulse = librosa.beat.plp(onset_envelope=onset_env, sr=sample_rate)
+    beats_plp = np.flatnonzero(librosa.util.localmax(pulse))
+
+    m_plp = np.mean(onset_env[beats_plp])
+    above_avg_plp = beats_plp[onset_env[beats_plp] >= m_plp]
+
+    peaks = beats[::4]
+    inner_peaks = np.delete(above_avg_plp, np.nonzero(np.in1d(above_avg_plp,
+                                                              peaks))[0])
+
+    peak_vals = (times[peaks] * sample_rate).astype(int)
+    slices = np.split(audio_data, peak_vals)[1:-1]
+    return onset_env, times, peaks, inner_peaks, peak_vals, slices, tempo, beats
+
+
+def random_arrangement(sliced_audio, min_mult, max_mult):
+    """
+    This function will arrange the sliced sections randomly.
+    """
+    rearranged = np.array([])
+
+    # shuffle slices
+    random.shuffle(sliced_audio)
+
+    # repeat and remix
+    rnd_int = np.random.randint(min_mult, max_mult, size=len(sliced_audio))
+    for slice, rnd in zip(sliced_audio, rnd_int):
+        repeat = np.tile(slice, rnd)
+        rearranged = np.append(rearranged, repeat)
+
+    return rearranged
 
 
 if __name__ == '__main__':
-    x, sr = librosa.load('reports/keys-of-moon-white-petals.wav')
-    onset_detection = OnsetDetection()
-    onset_envelope, fs = onset_detection.spectral_based_novelty(x, sr, H=512,
-                                                                M=100,
-                                                                norm=False)
-    tempo, peaks_sec = beat_tracking(x.size / sr, onset_envelope)
-    # tempo, peaks_sec = librosa.beat.beat_track(x, sr, units='time')
-    x_peaks = librosa.clicks(peaks_sec, sr=sr, click_freq=1000, length=len(x))
-    sf.write('test_guy2.wav', x + x_peaks, sr)
+    track_name = "Maarten Schellekens - Mallet Play"
+    audio_path = "samples/{}.wav".format(track_name)
+    audio_data, samp_rate = utils.get_wav_data(audio_path)
+    onset_env, times, peaks, inner_peaks, peak_vals, slices, tempo, beats = beat_tracking_and_slice(
+        audio_data, samp_rate)
+    utils.plot_reg_and_shuff_graphs(onset_env, [[peaks, "original peaks"],
+                                                [np.hstack(inner_peaks),
+                                                 "original inner peaks"],
+                                                [np.concatenate((np.array(
+                                                    peaks), np.hstack(
+                                                    inner_peaks))),
+                                                 "all peaks"]])
+    rearanged_slices = random_arrangement(slices, 1, 4)
+    shf_onset_env, shf_times, shf_peaks, shf_inner_peaks, shf_peak_vals, shf_slices, shf_tempo, shf_beats = beat_tracking_and_slice(
+        rearanged_slices, samp_rate)
+
+    utils.plot_reg_and_shuff_graphs(shf_onset_env, [[shf_peaks, "shuffled "
+                                                                "peaks"],
+                                                    [np.hstack(
+                                                        shf_inner_peaks),
+                                                     "shuffled inner peaks"],
+                                                    [np.concatenate((np.array(
+                                                        shf_peaks), np.hstack(
+                                                        shf_inner_peaks))),
+                                                     "all shuffled peaks"]])
+
+    utils.save_with_clicks(rearanged_slices, samp_rate, shf_peaks,
+                           np.hstack(shf_inner_peaks),
+                           "clicks_{}".format(track_name))
+
+    print("peaks times: ", list(shf_times[peaks]))
+    print("inner peaks times: ",
+          list(shf_times[np.hstack(inner_peaks).astype(int)]))
+    print(tempo)
+    print(times[-1])
+    print(len(shf_times))
+    print(shf_times[-1])
+    print(len(rearanged_slices))
+
+    sf.write('results/rearanged_9.wav', rearanged_slices, samp_rate)
--- utils.py
+++ utils.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from pydub import AudioSegment
-from scipy.io import wavfile
+import soundfile as sf
 import scipy.io
+import librosa
 
 
 def create_wav_file_from_mp3_file(src, dst):
@@ -12,3 +13,82 @@ def create_wav_file_from_mp3_file(src, dst):
     """
     sound = AudioSegment.from_mp3(src)
     sound.export(dst, format="wav")
+
+
+def get_wav_data(audio_path):
+    """
+    This function will track the beat and tempo, and will return an array of sliced sections accordingly.
+    """
+    audio_data, sample_rate = librosa.load(audio_path)
+    return audio_data, sample_rate
+
+
+def plot_beat_tracking_graphs(audio, samp_rate, times, onset_env, onset_fr, sign_changes,
+                              beats):
+    """
+    This function will plot desired graphs
+    """
+    fig, axs = plt.subplots(2)
+    axs[0].plot(audio)
+    axs[0].set_title("audio")
+    axs[1].plot(times * samp_rate, onset_env)
+    axs[1].vlines(times[onset_fr] * samp_rate, 0, onset_env.max(), 'r', linestyles='--')
+    axs[1].set_title("env")
+    plt.show()
+
+    S = np.abs(librosa.stft(audio))
+    fig, ax = plt.subplots()
+    img = librosa.display.specshow(librosa.amplitude_to_db(S, ref=np.max), y_axis='log', x_axis='time', ax=ax)
+    ax.set_title('Power spectrogram')
+    fig.colorbar(img, ax=ax, format="%+2.0f dB")
+    plt.show()
+
+    plt.plot(times, onset_env)
+    plt.vlines(times[onset_fr], 0, onset_env.max(), 'r', linestyles='--')
+    plt.show()
+
+    plt.plot(times, onset_env)
+    plt.vlines(times[np.argwhere(sign_changes > 0)], 0, onset_env.max(), 'r', linestyles='--')
+    plt.show()
+
+    Xdb = librosa.amplitude_to_db(abs(librosa.stft(audio)))
+    plt.figure(figsize=(14, 5))
+    librosa.display.specshow(Xdb, sr=samp_rate, x_axis='time', y_axis='hz')
+    #If to pring log of frequencies
+    librosa.display.specshow(Xdb, sr=samp_rate, x_axis='time', y_axis='log')
+    plt.colorbar()
+    plt.show()
+
+    # hop_length = 512
+    fig, ax = plt.subplots(nrows=2, sharex=True)
+    # times = librosa.times_like(onset_envelope, sr=sr, hop_length=hop_length)
+    M = librosa.feature.melspectrogram(y=audio, sr=samp_rate)
+    librosa.display.specshow(librosa.power_to_db(M, ref=np.max), y_axis='mel', x_axis='time', ax=ax[0])
+    ax[0].label_outer()
+    ax[0].set(title='Mel spectrogram')
+    ax[1].plot(times, librosa.util.normalize(onset_env), label='Onset strength')
+    ax[1].vlines(times[beats], 0, 1, alpha=0.5, color='r', linestyle='--', label='Beats')
+    ax[1].legend()
+    plt.show()
+
+
+def save_with_clicks(rearanged_slices, samp_rate, shf_peaks, shf_inner_peaks, name):
+    clicks = librosa.clicks(frames=shf_peaks, sr=samp_rate, length=len(rearanged_slices)) + librosa.clicks(frames=np.hstack(shf_inner_peaks), sr=samp_rate, length=len(rearanged_slices))
+    # clicks = librosa.clicks(frames=shf_peaks, sr=samp_rate, length=len(rearanged_slices))
+    # clicks = librosa.clicks(frames=np.hstack(shf_inner_peaks), sr=samp_rate, length=len(rearanged_slices))
+    y_beats = rearanged_slices + clicks
+    sf.write('results/{}.wav'.format(name), y_beats, samp_rate)
+
+
+def shorten_sample(audio_path, start_time_in_s, end_time_in_s, new_name):
+    audio_data, sm_rate = librosa.load(audio_path)
+    nw_sample = audio_data[(start_time_in_s * sm_rate):(end_time_in_s * sm_rate)]
+    sf.write('samples/{}.wav'.format(new_name), nw_sample, sm_rate)
+
+
+def plot_reg_and_shuff_graphs(envelope, graphs):
+    for g in graphs:
+        plt.plot(envelope)
+        plt.title(g[1])
+        plt.vlines(g[0], 0, envelope.max(), 'r', linestyles='--')
+        plt.show()
